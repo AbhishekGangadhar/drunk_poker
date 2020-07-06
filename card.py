12ce5b0d1f26df2dcf44170930690bb15265ec8d
AbhishekGangadhar <abhishekgangadhar95@gmail.com>
2020-07-06 17:18:53 +0530

    Adding Card class and unittests

---
+++ card.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+
+class Suit(Enum):
+    HEARTS = "HEARTS"
+    DIAMONDS = "DIAMONDS"
+    SPADES = "SPADES"
+    CLUBS = "CLUBS"
+
+
+class Rank(Enum):
+    ACE = 1
+    KING = 13
+    QUEEN = 12
+    JACK = 11
+    TEN = 10
+    NINE = 9
+    EIGHT = 8
+    SEVEN = 7
+    SIX = 6
+    FIVE = 5
+    FOUR = 4
+    THREE = 3
+    TWO = 2
+
+
+class Card:
+    def __init__(self, suit: Suit, rank: Rank):
+        self.__suit = suit
+        self.__rank = rank
+
+    def get_suit(self) -> Suit:
+        return self.__suit
+
+    def get_rank(self) -> Rank:
+        return self.__rank
+
+    def string(self) -> str:
+        return f"{self.__rank.name} of {self.__suit.value}"
+
+    def get_rank_val(self) -> int:
+        return self.__rank.value
+
---
+++ tests/test_cards.py
@@ -0,0 +1,34 @@
+import unittest
+from unittest import TestCase
+from card import Card, Suit, Rank
+
+
+class TestCards(TestCase):
+    def test(self):
+        ace_of_spades = Card(Suit.SPADES, Rank.ACE)
+        self.assertEqual(Suit.SPADES, ace_of_spades.get_suit())
+        self.assertEqual(Rank.ACE, ace_of_spades.get_rank())
+        self.assertEqual("ACE of SPADES", ace_of_spades.string())
+        self.assertEqual(1, ace_of_spades.get_rank_val())
+
+        two_of_hearts = Card(Suit.HEARTS, Rank.TWO)
+        self.assertEqual(Suit.HEARTS, two_of_hearts.get_suit())
+        self.assertEqual(Rank.TWO, two_of_hearts.get_rank())
+        self.assertEqual("TWO of HEARTS", two_of_hearts.string())
+        self.assertEqual(2, two_of_hearts.get_rank_val())
+
+        seven_of_clubs = Card(Suit.CLUBS, Rank.SEVEN)
+        self.assertEqual(Suit.CLUBS, seven_of_clubs.get_suit())
+        self.assertEqual(Rank.SEVEN, seven_of_clubs.get_rank())
+        self.assertEqual("SEVEN of CLUBS", seven_of_clubs.string())
+        self.assertEqual(7, seven_of_clubs.get_rank_val())
+
+        jack_of_diamonds = Card(Suit.DIAMONDS, Rank.JACK)
+        self.assertEqual(Suit.DIAMONDS, jack_of_diamonds.get_suit())
+        self.assertEqual(Rank.JACK, jack_of_diamonds.get_rank())
+        self.assertEqual("JACK of DIAMONDS", jack_of_diamonds.string())
+        self.assertEqual(11, jack_of_diamonds.get_rank_val())
+
+
+if __name__ == '__main__':
+    unittest.main()
